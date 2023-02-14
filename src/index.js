798063e9e1fdda07fa9fbe91ad84c99963ae0407
Bartosz777 <40851209+Bartosz777@users.noreply.github.com>
2023-02-14 13:05:07 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
